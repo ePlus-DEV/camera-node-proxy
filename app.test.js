cf17b56c15bbe4f5ea845fe502c17cb7ecac5cc5
David Nguyen <h250694@gmail.com>
2025-06-25 08:55:24 +0700

    update test

--- app.test.js
+++ app.test.js
@@ -3,14 +3,14 @@ const app = require('./app');
 
 describe('GET /camera', () => {
     test('missing id param returns 400', async () => {
-        const res = await request(app).get('/camera');
+        const res = await request(app).get('/camera').set('Origin', 'https://eplus.dev');
         expect(res.status).toBe(400);
         expect(res.text).toBe('Missing camera id');
     });
 
     test('valid id param returns 200 or 500 depending on server', async () => {
         const currentTime = Date.now();
-        const res = await request(app).get(`/camera?id=5a8241105058170011f6eaa6&bg=black&time=${currentTime}`).set('Origin', 'https://eplus.dev');;
+        const res = await request(app).get(`/camera?id=5a8241105058170011f6eaa6&bg=black&time=${currentTime}`).set('Origin', 'https://eplus.dev');
         expect([200, 500]).toContain(res.status);
     });
 });
